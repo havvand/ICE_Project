638d353166200c9020998a86f94f850ac4222a86
Hessdk <113091971+Hessdk@users.noreply.github.com>
2022-12-12 12:34:12 +0100

    Hello

---
+++ src/AgentBureau.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class AgentBureau {
+    int bureauLevel;
+    public AgentBureau(){
+
+    }
+
+
+    public int getBureauLevel() {
+        return bureauLevel;
+    }
+
+
+    public ArrayList<Clients> createPlayerClientPool(){
+        ArrayList<Client> playerClientPool = new ArrayList<Client>();
+
+
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         FileIO test = new FileIO();
         test.initiateClubs();
+        test.getALastName();
+        test.getAFirstName();
     }
 }
