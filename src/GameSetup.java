83fa419089dac8805d5e865cf5d28068332cc422
havvand <90390212+havvand@users.noreply.github.com>
2022-12-12 11:46:46 +0100

    Test

--- src/GameSetup.java
+++ src/GameSetup.java
@@ -1,2 +1,4 @@
-public class GameSetup {
+public class GameSetup
+{
+
 }
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main
+{
+    public static void main(String[] args) {
+        ClientPool pool = new ClientPool();
+
+        pool.addClient();
+    }
+}
