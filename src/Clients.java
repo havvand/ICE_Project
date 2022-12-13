36dc291491424b85ce3313a440ddaa8b34128ef7
Hessdk <113091971+Hessdk@users.noreply.github.com>
2022-12-13 11:29:28 +0100

    Hello

--- src/Clients.java
+++ src/Clients.java
@@ -1,4 +1,6 @@
+import java.util.Random;
 public class Clients
+
 {
     String firstName, lastName, position;
     int age, skill, potential, transferValue;
@@ -14,6 +16,27 @@ public class Clients
         this.transferValue = transferValue;
     }
 
+    public void nextTurn(){
+        Random rand = new Random();
+
+        age = age + 1;
+        if (potential >25 && potential <= 35){
+            int randInt = rand.nextInt(1)+1;
+            potential = potential + randInt;
+        } else if (potential >35 && potential <= 50) {
+            int randInt = rand.nextInt(5)+1;
+            potential = potential + randInt;
+        } else if (potential > 50 && potential <=65) {
+            int randInt = rand.nextInt(8)+1;
+            potential = potential + randInt;
+        } else if (potential > 65 && potential <= 80) {
+            int randInt = rand.nextInt(10)+1;
+            potential = potential + randInt;
+        } else if (potential > 80 && potential <=100) {
+            int randInt = rand.nextInt(13)+1;
+            potential = potential + randInt;
+        }
+    }
     public Clients newClient()
     {
         return new Clients("Ole", "Jensen", "top", 22, 44, 3, 1000000);
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,24 @@
+import java.util.Random;
+
 public class Main
 {
     public static void main(String[] args) {
+        int hej = 10;
         ClientPool pool = new ClientPool();
         TextUI ui = new TextUI();
         FileIO io = new FileIO();
+        Random rand = new Random();
+        System.out.println(hej);
+        int randomInt = rand.nextInt(100,)+1;
+        hej = hej + randomInt;
+        System.out.println(randomInt);
+        System.out.println(hej);
+
+
+
 
       //io.getAFirstName();
-      ui.printArray(io.initiateClubs());
+      //ui.printArray(io.initiateClubs());
        // io.getAFirstName();
     //  ui.printArray(io.firstNameList);
 
