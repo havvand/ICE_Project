b4a3b50c1b8f8b8b67f34d62e4d76d244af25e56
Salar86 <99397531+Salar86@users.noreply.github.com>
2022-12-13 11:12:44 +0100

    Second

---
+++ src/Bank.java
@@ -0,0 +1,32 @@
+public class Bank {
+    private int balance;
+    private int tax;
+
+
+
+    public int addMoney(int amountToAdd) {
+
+       return balance += amountToAdd;
+    }
+
+    public int withdrawMoney(int amountToWithdraw) {
+
+        return balance - amountToWithdraw;
+    }
+
+    public void taxes() {
+        balance -= tax;
+    }
+
+    public boolean isBankrupt() {
+        return getBalance() >= 0;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+}
