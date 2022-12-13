d9104329f01f7753cd37f08a0e7f757e2529389b
Hessdk <113091971+Hessdk@users.noreply.github.com>
2022-12-13 13:03:40 +0100

    Hello

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,6 @@ public class Main
         TextUI ui = new TextUI();
         FileIO io = new FileIO();
         Random rand = new Random();
-        System.out.println(hej);
-        int randomInt = rand.nextInt(100,)+1;
-        hej = hej + randomInt;
-        System.out.println(randomInt);
-        System.out.println(hej);
-
-
-
 
       //io.getAFirstName();
       //ui.printArray(io.initiateClubs());
