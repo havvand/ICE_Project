5e3131be5f6e4df08b16d64ee1d639d3e6f0f309
Hessdk <113091971+Hessdk@users.noreply.github.com>
2022-12-14 09:17:58 +0100

    Hejsa

--- src/GameSetup.java
+++ src/GameSetup.java
@@ -3,9 +3,11 @@ import java.util.Random;
 
 public class GameSetup
 {
+    FileIO io = new FileIO();
     Clients client = new Clients("","","",0,0,0,0, 0);
     public void nextTurn() {
-        FileIO io = new FileIO();
+
+
         Random rand = new Random();
         int upperLimit;
         int lowerLimit;
@@ -16,29 +18,40 @@ public class GameSetup
 
 
             if (client.getPotential() > 25 && client.getPotential() <= 35) {
-                int randInt = rand.nextInt(1) + 1;
-                client.setSkill(client.getSkill() + randInt);
+                if (client.getSkill()<35) {
+                    int randInt = rand.nextInt(1) + 1;
+                    client.setSkill(client.getSkill() + randInt);
 
+
+                }
             } else if (client.getPotential() > 35 && client.getPotential() <= 50) {
-                lowerLimit = 2;
-                upperLimit = 4;
-                int randInt = rand.nextInt(upperLimit - lowerLimit + 1) + lowerLimit;
-               client.setSkill(client.getSkill()+randInt);
+                if (client.getSkill() < 50) {
+                    lowerLimit = 2;
+                    upperLimit = 4;
+                    int randInt = rand.nextInt(upperLimit - lowerLimit + 1) + lowerLimit;
+                    client.setSkill(client.getSkill() + randInt);
+                }
             } else if (client.getPotential() > 50 && client.getPotential() <= 65) {
-                lowerLimit = 3;
-                upperLimit = 6;
-                int randInt = rand.nextInt(upperLimit - lowerLimit + 1) + lowerLimit;
-               client.setSkill(client.getSkill()+randInt);
+                if (client.getSkill() < 65) {
+                    lowerLimit = 3;
+                    upperLimit = 6;
+                    int randInt = rand.nextInt(upperLimit - lowerLimit + 1) + lowerLimit;
+                    client.setSkill(client.getSkill() + randInt);
+                }
             } else if (client.getPotential() > 65 && client.getPotential() <= 80) {
-                lowerLimit = 5;
-                upperLimit = 8;
-                int randInt = rand.nextInt(upperLimit - lowerLimit + 1) + lowerLimit;
-               client.setSkill(client.getSkill()+randInt);
+                if (client.getSkill() < 80) {
+                    lowerLimit = 5;
+                    upperLimit = 8;
+                    int randInt = rand.nextInt(upperLimit - lowerLimit + 1) + lowerLimit;
+                    client.setSkill(client.getSkill() + randInt);
+                }
             } else if (client.getPotential() > 80 && client.getPotential() <= 100) {
-                lowerLimit = 7;
-                upperLimit = 10;
-                int randInt = rand.nextInt(upperLimit - lowerLimit + 1) + lowerLimit;
-                client.setSkill(client.getSkill()+randInt);
+                if (client.getSkill()<95) {
+                    lowerLimit = 7;
+                    upperLimit = 10;
+                    int randInt = rand.nextInt(upperLimit - lowerLimit + 1) + lowerLimit;
+                    client.setSkill(client.getSkill() + randInt);
+                }
             }
         }
     }
