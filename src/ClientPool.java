81a2dbd2b63a15b7383b883c64bd9e358c3460e2
havvand <90390212+havvand@users.noreply.github.com>
2022-12-15 12:45:39 +0100

    Latest 141222_5: Kan nu skifte nogle af spillerene ud

--- src/ClientPool.java
+++ src/ClientPool.java
@@ -1,70 +1,63 @@
 import java.io.*;
 import java.util.ArrayList;
 
-public class ClientPool
-{
+public class ClientPool {
+    TextUI textUI = new TextUI();
     static ArrayList<Clients> clientPool = new ArrayList<>();
-    Clients client = new Clients("", "", "", 1,0, 0, 0, 0);
+    Clients client = new Clients("", "", "", 1, 0, 0, 0, 0);
     int goalKeeper, defender, midfield, forward;
 
-    public ClientPool()
-    {
+    public ClientPool() {
     }
 
-    public void initializeArrayList()
-    {
+    public void initializeArrayList() {
         clientPool = createClientPoolArray();
         System.out.println("INITALIZE ARRAYLIST " + clientPool.get(0).firstName);
     }
 
 
-    public void saveArrayToCsv()
-    {
+    public void saveArrayToCsv() {
         clearCsv();
 
-        for(int i = 0; i < 50; i++)
-        {
+        for (int i = 0; i < 50; i++) {
             try {
-                client = client.newClient(1, 0 );
+                client = client.newClient(1, 0);
                 //Scanner reader = new Scanner("data/userdata.txt");
                 FileWriter writer = new FileWriter("data/clientpool.csv", true);
-                writer.write(client.id + ";" + client.firstName + ";" + client.lastName + ";" + client.age + ";" + client.position + ";" +  client.skill + ";" +  client.potential + ";" + client.transferValue +";\n");
+                writer.write(client.id + ";" + client.firstName + ";" + client.lastName + ";" + client.age + ";" + client.position + ";" + client.skill + ";" + client.potential + ";" + client.transferValue + ";\n");
                 writer.close();
             } catch (IOException e) {
                 System.out.println(e);
             }
         }
-        for(int i = 50; i < 250; i++)
-        {
+        for (int i = 50; i < 250; i++) {
             try {
                 client = client.newClient(2, 0);
                 //Scanner reader = new Scanner("data/userdata.txt");
                 FileWriter writer = new FileWriter("data/clientpool.csv", true);
-                writer.write(client.id + ";" + client.firstName + ";" + client.lastName + ";" + client.age + ";" + client.position + ";" +  client.skill + ";" +  client.potential + ";" + client.transferValue +";\n");
+                writer.write(client.id + ";" + client.firstName + ";" + client.lastName + ";" + client.age + ";" + client.position + ";" + client.skill + ";" + client.potential + ";" + client.transferValue + ";\n");
                 writer.close();
             } catch (IOException e) {
                 System.out.println(e);
             }
         }
-        for(int i = 250; i < 450; i++)
-        {
+        for (int i = 250; i < 450; i++) {
             try {
                 client = client.newClient(3, 0);
                 //Scanner reader = new Scanner("data/userdata.txt");
                 FileWriter writer = new FileWriter("data/clientpool.csv", true);
-                writer.write(client.id + ";" + client.firstName + ";" + client.lastName + ";" + client.age + ";" + client.position + ";" +  client.skill + ";" +  client.potential + ";" + client.transferValue +";\n");
+                writer.write(client.id + ";" + client.firstName + ";" + client.lastName + ";" + client.age + ";" + client.position + ";" + client.skill + ";" + client.potential + ";" + client.transferValue + ";\n");
                 writer.close();
             } catch (IOException e) {
                 System.out.println(e);
             }
         }
-        for(int i = 450; i < 550; i++)
-        {
+        for (int i = 450; i < 550; i++) {
             try {
                 client = client.newClient(4, 0);
                 //Scanner reader = new Scanner("data/userdata.txt");
                 FileWriter writer = new FileWriter("data/clientpool.csv", true);
-                writer.write(client.id + ";" + client.firstName + ";" + client.lastName + ";" + client.age + ";" + client.position + ";" +  client.skill + ";" +  client.potential + ";" + client.transferValue +";\n");
+                writer.write(client.id + ";" + client.firstName + ";" + client.lastName + ";" + client.age + ";" + client.position + ";" + client.skill + ";" + client.potential + ";" + client.transferValue + ";\n");
                 writer.close();
             } catch (IOException e) {
                 System.out.println(e);
@@ -73,26 +66,21 @@ public class ClientPool
 
     }
 
-    public ArrayList<Clients> createClientPoolArray()
-    {
+    public ArrayList<Clients> createClientPoolArray() {
         ArrayList<Clients> clientPoolList = new ArrayList<>(); // The arraylist for the initialized clientpool!
 
-        for(int i = 0; i < 50; i++)
-        {
+        for (int i = 0; i < 50; i++) {
             clientPoolList.add(client.newClient(1, i));
         }
-        for(int i = 50; i < 250; i++)
-        {
+        for (int i = 50; i < 250; i++) {
             clientPoolList.add(client.newClient(2, i));
 
         }
-        for(int i = 250; i < 450; i++)
-        {
+        for (int i = 250; i < 450; i++) {
             clientPoolList.add(client.newClient(3, i));
         }
-        for(int i = 450; i < 550; i++)
-        {
-            clientPoolList.add(client.newClient(4,i));
+        for (int i = 450; i < 550; i++) {
+            clientPoolList.add(client.newClient(4, i));
         }
 
         /*for (Clients c : clientPoolList)
@@ -118,15 +106,13 @@ public class ClientPool
         return clientPoolList;
     }
 
-    private void countPlayerType()
-    {
+    private void countPlayerType() {
         goalKeeper = 0;
         defender = 0;
         midfield = 0;
         forward = 0;
 
-        for (Clients c : clientPool)
-        {
+        for (Clients c : clientPool) {
             if (c.position.equals("Goalkeeper")) {
                 goalKeeper++;
             }
@@ -156,70 +142,85 @@ public class ClientPool
     {
         countPlayerType();
 
-        if(goalKeeper < 50)
+        if (goalKeeper < 50)
         {
-            for(int i = clientPool.size(); i < 550; i++)
+            for (int i = clientPool.size(); i < 550; i++)
             {
-               clientPool.add(client.newClient(1, i));
+                clientPool.add(client.newClient(1, i));
             }
 
         }
-        if(defender < 200)
+        if (defender < 200)
         {
-            for(int i = clientPool.size(); i < 550; i++)
+            for (int i = clientPool.size(); i < 550; i++)
             {
                 clientPool.add(client.newClient(2, i));
                 System.out.println(clientPool.size());
             }
-        if(midfield < 200)
-        {
-            for(int i = clientPool.size(); i < 550; i++)
-            {
-                clientPool.add(client.newClient(3, i));
-                System.out.println(clientPool.size());
         }
 
-        if(forward < 100)
+        if (midfield < 200)
         {
-            for(int i = clientPool.size(); i < 550; i++)
+            for (int i = clientPool.size(); i < 550; i++)
             {
                 clientPool.add(client.newClient(3, i));
                 System.out.println(clientPool.size());
             }
         }
 
-       for(Clients c : clientPool)
-       {
-           System.out.println(c.position + " " + c.id + " " + c.firstName);
-       }
+        if (forward < 100)
+        {
+            for (int i = clientPool.size(); i < 550; i++)
+            {
+                clientPool.add(client.newClient(4, i));
+                System.out.println(clientPool.size());
+            }
+        }
 
-       countPlayerType();
+        for (Clients c : clientPool)
+        {
+            System.out.println(c.position + " " + c.id + " " + c.firstName);
+        }
+
     }
 
-    public void removeClientFromPool()
+
+    public void removeClientFromPool ()
     {
-        int input = clientPool.get(177).id; // REPLACE WITH USER INPUT
+        for (Clients c : clientPool)
+        {
+            System.out.println(c.position + " " + c.id + " " + c.firstName);
+        }
+        System.out.println(clientPool.size());
+        int input = textUI.getUserInputNum("Enter player ID to remove player!");
+                //clientPool.get(177).id; // REPLACE WITH USER INPUT
         //removes prospective client from clientPool as they're no longer available
-        for(int i = 0; i < clientPool.size(); ++i)
+        for (int i = 0; i < clientPool.size(); ++i)
         {
-            if(input == clientPool.get(i).id)
+            if (input == clientPool.get(i).id)
             {
+                System.out.println(clientPool.get(clientPool.size()).id+1);
                 clientPool.remove(clientPool.get(i));
-                break;
+                System.out.println(clientPool.get(i).id);
             }
-        }
 
+        }
+        System.out.println("SIZE AT " + clientPool.size());
+        for (Clients c : clientPool)
+        {
+            System.out.println(c.position + " " + c.id + " " + c.firstName);
+        }
     }
 
-    public static void clearCsv()
+
+    public static void clearCsv ()
     {
-        try
-        {
-        FileWriter writer = new FileWriter("data/clientpool.csv", false);
-        PrintWriter pw = new PrintWriter(writer, false);
-        pw.flush();
-        pw.close();
-        writer.close();
+        try {
+                    FileWriter writer = new FileWriter("data/clientpool.csv", false);
+                    PrintWriter pw = new PrintWriter(writer, false);
+                    pw.flush();
+                    pw.close();
+                    writer.close();
         } catch (IOException e)
         {
 
@@ -227,4 +228,5 @@ public class ClientPool
     }
 
 
-}
+
+    }
